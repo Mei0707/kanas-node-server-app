3ff569440f4f7bcb8a4420d6330e92aa1acbe448
Qiaowen Mei <meiqiaowen@meiqiaoendeiMac.attlocal.net>
2024-11-11 13:58:05 -0800

    signup

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
 
 const app = express();
-app.use(cors());
+UserRoutes(app);
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000']
+}
+));
 app.use(express.json());
-Hello(app);
+// Hello(app);
 Lab5(app);
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000, () => console.log('Server listening on port 4000'));
