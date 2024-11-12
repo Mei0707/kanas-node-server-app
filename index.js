b6c5f0b03397d176c3ff0cbb70db652f8740e112
Qiaowen Mei <meiqiaowen@meiqiaoendeiMac.attlocal.net>
2024-11-11 19:57:15 -0800

    add course

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+import "dotenv/config";
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import session from "express-session";
 
 const app = express();
 UserRoutes(app);
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000']
+    credentials: true,
+    // origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+
 }
 ));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUnintialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 // Hello(app);
+CourseRoutes(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000, () => console.log('Server listening on port 4000'));
