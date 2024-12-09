361a7756b562737ff07ac8438e1bb439e92764da
Qiaowen Mei <meiqiaowen@meiqiaoendeiMac.attlocal.net>
2024-12-08 19:54:15 -0800

    a6

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,30 +1,22 @@
-import courses from "../Database/courses.js";
-import Database from "../Database/index.js";
+import model from "./model.js";
 export function findAllCourses() {
-    return Database.courses;
+    return model.find();
 };
-export function findCoursesForEnrolledUser(userId) {
-    const { courses, enrollments } = Database;
-    const enrolledCourses = courses.filter((course) =>
-        enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
-    return enrolledCourses;
-};
+// export function findCoursesForEnrolledUser(userId) {
+//     const { courses, enrollments } = Database;
+//     const enrolledCourses = courses.filter((course) =>
+//         enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
+//     return enrolledCourses;
+// };
 export function createCourse(course) {
-    const newCourse = { ...courses, _id: Date.now().toString() };
-    Database.courses = [...Database.courses, newCourse];
-    return newCourse;
-};
-export function deleteCourse(courseId) {
-    const { courses, enrollments } = Database;
-    Database.courses = courses.filter((course) => course._id !== courseId);
-    Database.enrollments = enrollments.filter(
-        (enrollment) => enrollment.course !== courseId
-    );
+    delete course._id;
+    return model.create(course);
 };
 export function updateCourse(courseId, courseUpdates) {
-    const { courses } = Database;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
 };
+export function deleteCourse(courseId) {
+    return model.deleteOne({ _id: courseId });
+};
+   
 
--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,11 +1,28 @@
-import db from "../Database/index.js";
-let { users } = db;
-export const createUser = (user) => (users = [...users, { ...user, _id: Date.now() }]);
-export const findAllUsers = () => users;
-export const findUserById = (userId) => users.find((user) => user._id === userId);
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
-export const findUserByCredentials = (username, password) =>
-    users.find((user) => user.username === username && user.password === password);
-export const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+import model from "./model.js";
+// let { users } = db;
+export const createUser = (user) => { 
+    delete user._id;
+    return model.create(user);
+};
+export const findAllUsers = async (req, res) => {
+    const { role } = req.query;
+    if (role) {
+        const users = await dao.findUsersByRole(role);
+        res.json(users);
+        return;
+    }
+    const users = await dao.findAllUsers();
+    res.json(users);
+};
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (username) => model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+        $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+};
 
